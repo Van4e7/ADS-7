35934daa41a04be43f7f05765737304b48478e08
Van4e7 <ivanzvectov@gmail.com>
2025-05-02 09:56:36 +0300

    Update train.h

--- include/train.h
+++ include/train.h
@@ -1,4 +1,4 @@
-// Copyright 2022 NNTU-CS
+// Copyright 2022 NNTU-CS AND V47
 #ifndef INCLUDE_TRAIN_H_
 #define INCLUDE_TRAIN_H_
 
@@ -13,8 +13,17 @@ class Train {
   Car *first; // точка входа в поезд (первый вагон)
  public:
   Train();
+  ~Train();
   void addCar(bool light); // добавить вагон с начальным состоянием лампочки
   int getLength();          // вычислить длину поезда
   int getOpCount();         // вернуть число переходов (из вагона в вагон)
+  void resetOpCount();
+  void setAllLightsOff();
+  void setAllLightsOn();
+  void setRandomLights();
+  void moveNext(); 
+  void movePrev();
+  bool getLight();  
+  void toggleLight();
 };
 #endif  // INCLUDE_TRAIN_H_
